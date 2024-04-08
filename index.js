805a738305df38166660a88c82fccc49c380be29
GabrielR4SH <bieelsantana23@hotmail.com>
2024-04-07 23:39:37 -0300

    Request recive

--- index.js
+++ index.js
@@ -4,26 +4,28 @@ const url = require('url');
 const server = http.createServer((req, res) => {
     // Parsing request URL
     const parsedUrl = url.parse(req.url, true);
+    const parsedReq = {};
 
-    const parsedReq = {
-        path: parsedUrl.pathname,
-        method: req.method.toLowerCase(),
-        headers: req.headers,
-        queryStringObject: parsedUrl.query
-    };
-
-    // Remove the leading slash (/) from the path
+    parsedReq.parsedUrl = url.parse(req.url, true);
+    parsedReq.path = parsedReq.parsedUrl.pathname;
     parsedReq.timmedPath = parsedReq.path.replace(/^\//, '');
+    parsedReq.method = req.method.toLowerCase();
+    parsedReq.headers = req.headers;
+    parsedReq.queryStringObject = parsedReq.parsedUrl.query;
 
-    // Sending response
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(`
-    Path: ${parsedReq.path}
-    Timmed Path: ${parsedReq.timmedPath}
-    Method: ${parsedReq.method}
-    Headers: \n${JSON.stringify(parsedReq.headers, null, 2)}
-    Query Object: \n${JSON.stringify(parsedReq.queryStringObject, null, 2)}
-    `);
+    let body = [];
+
+    req.on('data',(chunk) => {
+        body.push(chunk);
+    });
+
+    req.on('end',() => {
+        body = Buffer.concat(body).toString();
+        parsedReq.body = body;
+        res.end(parsedReq.body);
+    });
+
+
 });
 
 server.listen(3000, () => console.log("Listening on port 3000"));
